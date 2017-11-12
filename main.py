eab3d60dd6cfb9a62d86ea87d91e727b692a4c84
srini akhil <sriniakhil13@gmail.com>
2017-11-12 15:24:04 +0530

    main.py
    Get tweets of particular object/event/personality from twitter and analyze sentiments/peoples reactions

---
+++ main.py
@@ -0,0 +1,14 @@
+import re
+''' regexp module'''
+
+import tweepy 
+'''Tweepy module enables Python to communicate with Twitter platform and use its API'''
+
+from tweepy import OAuthHandler
+''' To authenticate twitter and get access to tweets posted'''
+
+from textblob import TextBlob
+'''It is natural language processing toolkit/module'''
+
+
+	
